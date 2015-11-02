3158e7275a493fd4bd1fe0defd03eeee9e5c66c1
terryshek <terryshek@msn.com>
2015-11-02 17:42:50 +0800

    timewaste

--- app/app.js
+++ app/app.js
@@ -2,6 +2,13 @@
  * Created by terryshek on 2/11/15.
  */
 (function(){
-    angular.module("TimeWaste", []);
+    angular.module("TimeWaste", ['ui.router'])
+        .config(function($stateProvider){
+            $stateProvider.state('signUp',{
+                url:"/signUp",
+                templateUrl:"app/signup/signup.html",
+                controller:"SignUpController"
+            })
+        })
 
 }());
---
+++ app/signup/signup-controller.js
@@ -0,0 +1,6 @@
+(function(){
+    angular.module("TimeWaste").controller("SignUpController", ["$scope", "$state", function($scope, $state){
+
+
+    }])
+}());
